35de25279e10687124a0f64da677af18107f5262
dalpat raj <dalpatt1212@gmail.com>
2024-12-15 16:15:58 +0530

    collction user side

--- app/ui/collections/CollProdCard.tsx
+++ app/ui/collections/CollProdCard.tsx
@@ -13,21 +13,21 @@ const CollProdCard: React.FC<CollectionProps> = ({collections}) => {
     
   return (
     <Link href={`/collections/${collections?.title}`}>
-            <div className='collll h-[50vh] max-sm:h-[30vh] relative rounded-lg overflow-hidden'>
-            <div className=''>
-                {/* <Image
-                    src={imageUrl1}
-                    alt={imageUrl1}
-                    fill
-                    sizes='100vw'
-                    style={{objectFit: 'cover'}}
-                    unoptimized
-                /> */}
-            </div>
-            <div className='second transition duration-200 ease-in delay-200 h-full w-full bg-blackOverlay absolute top-0 left-0 flext items-center justify-center '>
-                <p className='text-[28px] text-white  font-bold absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>{collections?.title}</p>
-            </div>
-            </div>
+      <div className='collll h-[50vh] max-sm:h-[30vh] relative rounded-lg overflow-hidden'>
+        <div className=''>
+            <Image
+                src={imageUrl1}
+                alt={imageUrl1}
+                width={0}
+                height={0}
+                sizes='100vw'
+                style={{width: "100%", height: "100%", objectFit: 'cover'}}
+            />
+        </div>
+        <div className='second transition duration-200 ease-in delay-200 h-full w-full bg-blackOverlay absolute top-0 left-0 flext items-center justify-center '>
+            <p className='text-[28px] text-white  font-bold absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>{collections?.title}</p>
+        </div>
+      </div>
     </Link>
   )
 }
