8c2eb87b92aaa1847ce1819900f179838cb7cde5
dalpat raj <dalpatt1212@gmail.com>
2024-12-23 21:22:37 +0530

    vercel guidline so i update prisma schema

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,24 +1,24 @@
-import { db } from "@/lib/db";
-import { productData } from "@/lib/placeholder_data";
+// import { db } from "@/lib/db";
+// import { productData } from "@/lib/placeholder_data";
 
 
 
-async function main() {
-    // Insert products into the database
-    for (const product of productData) {
-      await db.product.create({
-        data: product
-      });
-    }
+// async function main() {
+//     // Insert products into the database
+//     for (const product of productData) {
+//       await db.product.create({
+//         data: product
+//       });
+//     }
   
-    console.log("Seed data inserted");
-  }
+//     console.log("Seed data inserted");
+//   }
   
-  main()
-    .catch((e) => {
-      console.error(e);
-      process.exit(1);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
+//   main()
+//     .catch((e) => {
+//       console.error(e);
+//       process.exit(1);
+//     })
+//     .finally(async () => {
+//       await prisma.$disconnect();
+//     });
