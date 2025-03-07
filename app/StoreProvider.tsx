7bd4be5d0f80c3bce3c81313e33085f8446dd93c
dalpat raj <dalpatt1212@gmail.com>
2025-03-07 14:16:51 +0530

    some changes

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode, useRef } from 'react';
 import { Provider } from 'react-redux';
 
 const StoreProvider = ({ children }: { children: ReactNode }) => {
-    const storeRef = useRef<AppStore>();
+    const storeRef = useRef<AppStore>(undefined);
 
 
     if (!storeRef.current) {
--- app/ui/homePage/IphoneZone.tsx
+++ app/ui/homePage/IphoneZone.tsx
@@ -12,7 +12,7 @@ const IphoneZone = async() => {
     <div className='w-full py-8 max-sm:py-6 px-12 max-md:px-4 max-sm:px-2'>
         <div className='flex justify-between items-center mb-8  max-sm:mb-4'>
             <h2 className={`${caveat.className} text-[37px] font-bold max-sm:text-[24px]`}>I Phone Zone</h2>
-            <button className='border border-gray-200 rounded-md px-3 py-2 text-[14px] font-semibold'><Link href={"/products/samsung"}>View More</Link></button>
+            <button className='border border-gray-200 rounded-md px-3 py-2 text-[14px] font-semibold'><Link href={"/products"}>View More</Link></button>
         </div>
         <div>
           <ProductSlider products={products}/>
